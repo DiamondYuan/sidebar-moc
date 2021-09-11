7819b95f8b0be837b53728fc32b2c7ceb54d6de7
DiamondYuan <admin@diamondyuan.com>
2021-09-11 14:47:14 +0800

    feat: support open url

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,114 @@
+import { OutlineContent, OutlineRoot } from "./types";
 import vscode from "vscode";
 import { transformMdastToMocAst } from "./transform";
 import { sortRoot } from "./sort";
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
-
+import { Utils } from "vscode-uri";
 class TreeViewItem extends vscode.TreeItem {
-  constructor(data: { title: string; link: string }) {
-    super(data.title);
+  constructor(element: OutlineContent, context: vscode.ExtensionContext) {
+    super(
+      element.text ?? "NONE",
+      element.children.length === 0
+        ? vscode.TreeItemCollapsibleState.None
+        : vscode.TreeItemCollapsibleState.Expanded
+    );
+
+    this.iconPath = vscode.Uri.joinPath(
+      context.extensionUri,
+      "resource/icon/url.svg"
+    );
+    this.command = {
+      command: "sidebar-moc.open-uri",
+      title: "Open",
+      arguments: [element.url],
+    };
   }
 }
 
-class DataProvider implements vscode.TreeDataProvider<TreeViewItem> {
-  constructor() {}
+class DataProvider implements vscode.TreeDataProvider<OutlineContent> {
+  constructor(
+    private context: vscode.ExtensionContext,
+    private root: OutlineContent | OutlineRoot
+  ) {}
+
+  private _onDidChangeTreeData: vscode.EventEmitter<undefined | void> =
+    new vscode.EventEmitter<undefined | void>();
+  readonly onDidChangeTreeData: vscode.Event<undefined | void> =
+    this._onDidChangeTreeData.event;
+
+  refresh(root: OutlineContent): void {
+    this.root = root;
+    this._onDidChangeTreeData.fire();
+  }
+
   getTreeItem(
-    element: TreeViewItem
+    element: OutlineContent
   ): vscode.TreeItem | Thenable<vscode.TreeItem> {
-    return element;
+    return new TreeViewItem(element, this.context);
   }
-  getChildren(element?: TreeViewItem): vscode.ProviderResult<TreeViewItem[]> {
-    return [];
+
+  getChildren(
+    element?: OutlineContent
+  ): vscode.ProviderResult<OutlineContent[]> {
+    if (!element) {
+      return this.root.children;
+    }
+    return element.children;
   }
 }
 
-export function activate(context: vscode.ExtensionContext) {
-  vscode.window.createTreeView("sidebar-moc.view", {
-    treeDataProvider: new DataProvider(),
-  });
-  const channel = vscode.window.createOutputChannel("Sidebar MOC");
-  const ew = vscode.workspace.getConfiguration();
-  channel.appendLine(ew.get("sidebar-moc.mocPath")!);
-  vscode.workspace
-    .openTextDocument(vscode.Uri.file(ew.get("sidebar-moc.mocPath")!))
-    .then((doc) => {
+function loadAndParse(url: string): Promise<OutlineContent> {
+  return new Promise<OutlineContent>((r) => {
+    vscode.workspace.openTextDocument(vscode.Uri.file(url)).then((doc) => {
       const mdast = unified()
         .use(remarkParse)
         .use(remarkGfm)
         .parse(doc.getText());
-      console.log(sortRoot(transformMdastToMocAst(mdast) as any));
+      const root = sortRoot(transformMdastToMocAst(mdast) as any) as any;
+      r(root);
     });
+  });
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+  const channel = vscode.window.createOutputChannel("Sidebar MOC");
+  const config = vscode.workspace.getConfiguration();
+  const mocPath: string | undefined = config.get("sidebar-moc.mocPath");
+  const treeDataProvider = new DataProvider(context, {
+    type: "root",
+    children: [],
+  });
+  vscode.window.createTreeView("sidebar-moc.view", {
+    treeDataProvider: treeDataProvider,
+  });
+
+  if (mocPath) {
+    const root = await loadAndParse(mocPath);
+    treeDataProvider.refresh(root);
+  }
+
+  vscode.commands.registerCommand("sidebar-moc.open-uri", (file?: string) => {
+    if (!mocPath) {
+      return;
+    }
+    const mocURI = vscode.Uri.file(mocPath);
+    if (!file) {
+      return;
+    }
+    const fileUrI = vscode.Uri.joinPath(Utils.dirname(mocURI), file);
+    openAndShow(fileUrI);
+  });
+}
+
+function openAndShow(file: vscode.Uri) {
+  vscode.workspace.openTextDocument(file).then(
+    (a: vscode.TextDocument) => {
+      vscode.window.showTextDocument(a, 1, false);
+    },
+    (error: any) => {
+      //
+    }
+  );
 }
