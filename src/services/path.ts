1c9706804ed7db5d6f83b2f258037061a9092779
DiamondYuan <admin@diamondyuan.com>
2021-10-02 23:57:07 +0800

    feat: update PathService

--- src/services/path.ts
+++ src/services/path.ts
@@ -2,5 +2,16 @@ import vscode from "vscode";
 
 export interface IPathService {
   uriToConfigPath(fs: vscode.Uri): string;
-  ConfigPathToUri(configPath: string): vscode.Uri;
+  configPathToUri(configPath: string): vscode.Uri;
+}
+
+export class PathService implements IPathService {
+  constructor(private base: vscode.Uri) {}
+  uriToConfigPath(fs: vscode.Uri) {
+    return "";
+  }
+
+  configPathToUri(configPath: string): vscode.Uri {
+    return vscode.Uri.joinPath(this.base, configPath);
+  }
 }
